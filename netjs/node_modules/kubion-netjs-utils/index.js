84c020b3fb0e5899e5b81f444208910f3f6483b9
Merel de Klein <mereldeklein@live.nl>
2019-01-28 09:07:55 +0100

    Split in to netjs & utils, added Obj + Format files & Topic Classification

---
+++ netjs/node_modules/kubion-netjs-utils/index.js
@@ -0,0 +1,7 @@
+const Format = require("./src/format.js");
+const Obj = require("./src/obj.js");
+
+module.exports = {
+	Format,
+	Obj
+};
---
+++ netjs/src/lib/text.js
@@ -0,0 +1,36 @@
+class Text {
+
+    static stripHtml (html) {
+        html = html.replace(/<\/(h1|h2|h3|h4|h5|p|div)>|<br ?\/?>/g, "\n");
+        html = html.replace(/<[^>]*>/g, "");
+        html = html.replace(/&nbsp;/, " ");
+
+        // Remove all whitespace before and after text
+        html = html.trim();
+
+        // Remove all triple newlines
+        html = html.replace(/\n \n/g, "\n\n");
+        html = html.replace(/\n\n\n/g, "\n\n");
+        
+        return html;
+    }
+
+    static decodeEntities (encodedString) {
+        var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
+        var translate = {
+            "nbsp":" ",
+            "amp" : "&",
+            "quot": "\"",
+            "lt"  : "<",
+            "gt"  : ">"
+        };
+        return encodedString.replace(translate_re, function(match, entity) {
+            return translate[entity];
+        }).replace(/&#(x?[0-9A-F]+);/gi, function(match, numStr) {
+            var num = numStr.startsWith("x") ? parseInt(numStr.substr(1), 16) : parseInt(numStr, 10);
+            return String.fromCharCode(num);
+        });
+    }
+}
+
+module.exports = Text;
